7e71de3ac5c4bd0ebc3b947b32d45833151fe55c
dingjiayuan <dingjiayuan13@gmail.com>
2024-11-05 10:47:42 +0800

    solve modules

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -12,7 +12,7 @@ fn compose_me(input: &str) -> String {
 }
 
 fn replace_me(input: &str) -> String {
-    input .replace("cars", "balloons")
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -34,7 +34,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
